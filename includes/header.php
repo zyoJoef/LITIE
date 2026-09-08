5a2acefa29861b16a14ccf48c179c60e5eade7ea
zyoJoef <cadornajoeffrey9@gmail.com>
2026-09-08 16:35:09 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,53 +1,5 @@
-<?php
-
-$isActivity =
-strpos(
-$_SERVER['PHP_SELF'],
-'activity/'
-)!==false;
-
-$path =
-$isActivity
-?
-'../'
-:
-'';
-
-?>
-
-<header class="header">
-
-    <!-- FOR THE CLICKABLE LOGO AND TITLE -->
-
-    <a href="<?= $path ?>index.php" class="home-link">
-        <div class="header-left">
-            <img src="<?= $path ?>pictures/logo.jpg" alt="Portfolio Logo" class="logo">
-            <h1>Cadorna's Portfolio</h1>
-        </div>
-    </a>
-
-    <nav class="nav-box" id="navMenu">
-        <a href="<?= $path ?>index.php">Home</a>
-        <a href="<?= $path ?>about.php">About</a>
-        <a href="<?= $path ?>project.php">Project</a>
-        <a href="<?= $path ?>contact.php">Contact</a>
-    </nav>
-
-</header>
-
-<script>
-
-function toggleMenu(){
-
-document
-.getElementById(
-"navMenu"
-)
-.classList
-.toggle(
-"active"
-);
-
-}
-
-</script>
+    <header>
+        <h1><a href="index.html">My Digital Life Journey</a></h1>
+        <p>Living In The I.T. Era</p>
+        <p><strong>Digital Portfolio</strong></p>
+    </header>
