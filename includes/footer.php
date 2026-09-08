52a1e383d3ba44f0753af83c797d293c52a20f59
zyoJoef <cadornajoeffrey9@gmail.com>
2026-09-08 16:36:44 +0800

    Delete includes directory

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer>
-        <p><strong>My Digital Life Journey</strong></p>
-        <p>Digital Portfolio | Living In The I.T. Era</p>
-        <p>&copy; 2026 My Digital Portfolio</p>
-    </footer>
--- includes/header.php
+++
@@ -1,5 +0,0 @@
-    <header>
-        <h1><a href="index.html">My Digital Life Journey</a></h1>
-        <p>Living In The I.T. Era</p>
-        <p><strong>Digital Portfolio</strong></p>
-    </header>
